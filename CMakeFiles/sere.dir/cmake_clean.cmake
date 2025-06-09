1b75779a2f302509485be262760b9953015f2f47
jj <118225440+youthx@users.noreply.github.com>
2025-06-09 06:34:48 +0000

    wqqwe

---
+++ CMakeFiles/sere.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sere.dir/src/main.cpp.o"
+  "CMakeFiles/sere.dir/src/main.cpp.o.d"
+  "sere"
+  "sere.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sere.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
